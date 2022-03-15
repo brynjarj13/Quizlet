908b54189e68847b62704844f3b735365ab54f9e
jso8 <56092916+jso8@users.noreply.github.com>
2022-03-15 18:44:35 +0000

    loginactivity listeners
    setting up buttons and listeners code

--- app/src/main/java/is/hi/hbv601g/quizlet/LoginActivity.java
+++ app/src/main/java/is/hi/hbv601g/quizlet/LoginActivity.java
@@ -1,5 +1,43 @@
 package is.hi.hbv601g.quizlet;
 
-public class LoginActivity {
+import android.view.View;
+import android.widget.Button;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button loginB;
+    private Button registerB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginB = (Button) findViewById(R.id.loginButton);
+        loginB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                private boolean loginFailed = false;
+                if(!loginFailed){
+                    Toast.makeText(LoginActivity.this, R.string.error_login, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        registerB = (Button) findViewById(R.id.registerButton);
+        registerB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
+    }
 
 }
