66c50d6cbcaa12c0a3651ba687a7e27896343f2c
adamelid <adamelid16@gmail.com>
2022-03-17 23:19:32 +0000

    Created navigation menu
    Created navigation menu and added to login.

--- app/src/main/java/is/hi/hbv601g/quizlet/LoginActivity.java
+++ app/src/main/java/is/hi/hbv601g/quizlet/LoginActivity.java
@@ -1,13 +1,17 @@
 package is.hi.hbv601g.quizlet;
 
 import android.content.Intent;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,6 +21,8 @@ public class LoginActivity extends AppCompatActivity {
     private EditText passwordT;
 
 
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
     public boolean Login(String textUserID, String textPasswordID) {
 
         return true;
@@ -25,6 +31,19 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // drawer layout instance to toggle the menu icon to open
+        // drawer and back button to close drawer
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         userT = (EditText) findViewById(R.id.editTextTextPersonName);
         passwordT = (EditText) findViewById(R.id.editTextTextPassword2);
@@ -44,7 +63,6 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
-
         registerB = (Button) findViewById(R.id.registerButton);
         registerB.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,9 +70,19 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+    }
+    // override the onOptionsItemSelected()
+    // function to implement
+    // the item click listener callback
+    // to open and close the navigation
+    // drawer when the icon is clicked
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-
-
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
