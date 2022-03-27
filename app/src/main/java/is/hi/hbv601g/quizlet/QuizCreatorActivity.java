b53e261dc7e3cfa279e8fb33ecffe27b64c41022
jso8 <56092916+jso8@users.noreply.github.com>
2022-03-27 02:31:14 +0000

    Basic setup for quizcreator
    added buttons and edittext
    added basic onclick for buttons

--- app/src/main/java/is/hi/hbv601g/quizlet/QuizCreatorActivity.java
+++ app/src/main/java/is/hi/hbv601g/quizlet/QuizCreatorActivity.java
@@ -1,4 +1,66 @@
 package is.hi.hbv601g.quizlet;
 
-public class QuizCreatorActivity {
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QuizCreatorActivity extends AppCompatActivity{
+
+    private Button newQuizB;
+    private Button myQuizsb;
+    private Button saveQuestion;
+    private EditText quizQuestion;
+    private EditText quizAnswer;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quizcreator);
+
+        quizQuestion = (EditText) findViewById(R.id.newQuizQuestion);
+        quizAnswer = (EditText) findViewById(R.id.newQuizAnswer);
+
+        newQuizB = (Button) findViewById(R.id.NewQuizButton);
+        newQuizB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // hide my quiz button and new quiz button
+                myQuizsb.setVisibility(View.GONE);
+                newQuizB.setVisibility(View.GONE);
+                boolean loginFailed = false;
+                if(loginFailed){
+                    Toast.makeText(LoginActivity.this, R.string.error_login, Toast.LENGTH_SHORT).show();
+                }else{
+                    // go to main activity
+                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(i);
+                }
+            }
+        });
+
+        myQuizsb = (Button) findViewById(R.id.myQuizsButton);
+        myQuizsb.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+
+                // hide new quiz button and new quiz button
+                newQuizB.setVisibility(View.GONE);
+                myQuizsb.setVisibility(View.GONE);
+
+            }
+        });
+
+
+
+    }
 }
